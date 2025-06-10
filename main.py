ff344dda48057aab977f7bc04e7c48ed2e8c6727
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-06-10 11:27:35 +1000

    Add GameView class with init variables

--- main.py
+++ main.py
@@ -2,3 +2,31 @@ import arcade
 import math
 
 from CONSTANTS import *
+
+class GameView(arcade.Window):
+    """"
+    Main application class.
+    """
+    
+    def __init__(self):
+        # Set up window using paretn class
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        
+        # Set up class variables
+        # Player
+        self.player_texture = None
+        self.player_sprite = None
+        
+        # Player Movement
+        self.player_movement_speed = PLAYER_MOVEMENT_SPEED
+        self.moving_forward = False
+        self.moving_backward = False
+        self.turning_left = False
+        self.turning_right
+        
+        # Camera
+        self.camera = None
+        self.gui_camera = None
+        
+        # Map
+        self.tile_map = None
