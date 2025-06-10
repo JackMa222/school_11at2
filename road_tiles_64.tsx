9a4c7b216e93287af14230ccf16ca4146398ff0b
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-06-10 11:46:47 +1000

    Add required resources/map files

---
+++ road_tiles_64.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="road_tiles_64" tilewidth="64" tileheight="64" tilecount="78" columns="13">
+ <image source="../Resources/road_tiles_64.png" width="832" height="384"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.181818" y="0.727273">
+    <polygon points="0,0 63.0909,63.2727 63.8182,63.2727 64.5455,63.0909 64.3636,57.2727 6.54545,-0.727273 0.150568,-0.696023"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0869565" y="58.8261" width="5.21739" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0.118087" y="5.13677">
+    <polygon points="0,0 11.5134,0.88565 34.7765,3.9559 53.139,8.20702 62.2317,11.3954 63.8258,10.864 63.8849,4.66442 52.6666,1.65321 36.5478,-2.30269 23.2631,-4.31016 16.8273,-5.19581 -0.118087,-5.07772"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.733662" y="17.5239">
+    <polygon points="-0.649476,-0.118087 19.8107,8.31617 40.8442,20.9766 54.9073,32.2717 63.2651,40.6745 63.206,29.9724 47.3677,17.3891 16.1084,-1.47739 8.56016,-3.9719 -0.61144,-6.82671"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.546942" y="56.0242">
+    <polygon points="-0.767563,0.295217 7.54464,8.06566 15.2691,8.01196 14.1211,6.04783 6.15536,-2.71535 -0.536604,-8.13538"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.17713" y="0.0684194" width="64.3636" height="5.09091"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="63.6364">
+    <polygon points="-0.0590433,0.35426 58.2859,-58.059 57.8182,-63.6364 56.5455,-63.6364 -5.81349,-0.545455 -5.88191,0.358948"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0434783" width="5" height="63.9565"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.26177" y="63.4385">
+    <polygon points="-0.295217,0.590433 25.8668,0.500549 19.3792,-2.98945 8.27879,-4.90721 -0.0930201,-4.85081"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.9375" y="0.422447">
+    <polygon points="-0.826606,-0.41676 13.1046,15.9806 21.4738,28.4416 30.6797,45.2731 36.1349,57.0406 38.8978,63.6088 45.4688,63.7338 44.2612,58.4329 33.2683,33.4804 18.5346,11.6545 8.67867,0.121989 8.36923,-0.156822 8.02726,-0.424436"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="46" y="-0.0434783" width="18.3913" height="4.78261"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.03125" y="6.03125">
+    <polygon points="0,0 7.90625,-6.0625 -0.09375,-6"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0" width="5.17391" height="18.1304"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="63.4063" y="57.6563">
+    <polygon points="0.59375,1.8125 -0.42778,1.75171 -2.20011,-6.10532 -4.61345,-16.6559 -8.86082,-25.8355 -19.3621,-38.6556 -30.1795,-47.2841 -45.7674,-54.3406 -58.5,-56.1797 -58.8454,-57.6875 -34.1793,-57.7523 -22.0896,-50.2039 -11.2422,-41.0005 -3.09349,-29.3988 -0.85977,-25.2269 0.625525,-21.0806"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.08889" y="63.1875">
+    <polygon points="-0.0625,0.90625 -0.055435,-9.39807 -0.962322,-15.4809 -2.86691,-22.2753 -5.01736,-24.5131 -5.07102,0.926749"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="48.0435" y="0.627363">
+    <polygon points="-0.375,2.25 5.38995,21.2801 8.98098,42.5635 10.7891,63.3508 15.9877,63.3727 15.9454,46.079 13.4121,25.676 8.59541,6.28053 6.02388,-0.640557 -0.609884,-0.659394"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
