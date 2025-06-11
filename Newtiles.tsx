bffdabce0f9fe50074b84e53c1d5fc520293e8f7
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-06-11 15:03:21 +1000

    Add ned maps and map images to repo

---
+++ Newtiles.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Newtiles" tilewidth="64" tileheight="64" tilecount="78" columns="13">
+ <image source="Resources/Newtiles.png" width="832" height="383"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="64">
+    <polygon points="0,0 5.0625,0 5,-59 64.0625,-59 64.0625,-64.0625 -0.0625,-64"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.1875" y="-0.0625">
+    <polygon points="0,0 64.3125,0.0625 64.4375,5.25 0.125,5.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
