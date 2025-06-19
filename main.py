cbd7fa1b2459cedc28d8291f7ef2312f584fe429
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-06-19 11:47:00 +1000

    Update movement to be based on time not frames

--- main.py
+++ main.py
@@ -51,14 +51,14 @@ class GameView(arcade.View):
         
         self.timer_text = None
     
-    def update_movement(self):
+    def update_movement(self, delta_time):
         radians_angle = math.radians(self.player_sprite.angle)
         if self.moving_forward:
-            self.player_sprite.change_x = self.player_movement_speed * math.sin(radians_angle)
-            self.player_sprite.change_y = self.player_movement_speed * math.cos(radians_angle)
+            self.player_sprite.change_x = self.player_movement_speed * math.sin(radians_angle) * delta_time
+            self.player_sprite.change_y = self.player_movement_speed  * math.cos(radians_angle) * delta_time
         elif self.moving_backward:
-            self.player_sprite.change_x = -self.player_movement_speed * math.sin(radians_angle)
-            self.player_sprite.change_y = -self.player_movement_speed * math.cos(radians_angle)
+            self.player_sprite.change_x = -self.player_movement_speed * math.sin(radians_angle) * delta_time
+            self.player_sprite.change_y = -self.player_movement_speed * math.cos(radians_angle) * delta_time
         else:
             self.player_sprite.change_x = 0
             self.player_sprite.change_y = 0
@@ -69,18 +69,21 @@ class GameView(arcade.View):
         # Center camera to player
         self.camera.position = self.player_sprite.position
         
+        # Update movement
+        self.update_movement(delta_time)
+        
         # Continue turning based on attributes
         # If still turning left, turn sprite left
         if self.turning_left:
             self.player_sprite.angle -= TURN_SPEED
             # Update movement to ensure moving forwards/backwards reflects the new angle
-            self.update_movement()
+            self.update_movement(delta_time)
         
         # If still turning right, turn sprite right
         if self.turning_right:
             self.player_sprite.angle += TURN_SPEED
             # Update movement to ensure moving forwards/backwards reflects the new angle
-            self.update_movement()
+            self.update_movement(delta_time)
         
         # Check if player is 'going' in the map
         # If so update timer
@@ -111,47 +114,37 @@ class GameView(arcade.View):
         if key == arcade.key.W or key == arcade.key.UP:
             # Move forward in the direction the car is facing
             self.moving_forward = True
-            self.update_movement()
         if key == arcade.key.A or key == arcade.key.LEFT:
             # Rotate car left and update movement
             self.turning_left = True
-            self.update_movement()
         elif key == arcade.key.D or key == arcade.key.RIGHT:
             # Rotate car right and update movement
             self.turning_right = True
-            self.update_movement()
         elif key == arcade.key.S or key == arcade.key.DOWN:
             # Move car backwards
             self.moving_backward = True
-            self.update_movement()
         elif key == arcade.key.SPACE:
             # Ability to increase speed on space press
             if IS_TURBO:
                 self.player_movement_speed = PLAYER_TURBO_MOVEMENT_SPEED
-            self.update_movement()
             
     def on_key_release(self, key, modifers):
         # When keys are released the movement and/or turning also ceases
         if key == arcade.key.W or key == arcade.key.UP:
             # Stop moving forward when W or UP is released
             self.moving_forward = False
-            self.update_movement()
         elif key == arcade.key.S or key == arcade.key.DOWN:
             # Stop moving backwards when S or DOWN is released
             self.moving_backward = False
-            self.update_movement()
         elif key == arcade.key.A or key == arcade.key.LEFT:
             # Stop turning left when A or LEFT is released
             self.turning_left = False
-            self.update_movement()
         elif key == arcade.key.D or key == arcade.key.RIGHT:
             # Stop turning right when D or RIGHT is released
             self.turning_right = False
-            self.update_movement()
         elif key == arcade.key.SPACE:
             # Revert player_movement speed to initial value once SPACE is released
             self.player_movement_speed = PLAYER_MOVEMENT_SPEED
-            self.update_movement()
             
     def setup(self):
         # Inital/reset code
